b8735a1c354f0f148db94845a713c91891f18a2a
dylan crocker and Marissa Perry <dylan@>
2019-03-26 11:10:17 -0700

    add toggle list

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $().ready(function(){
-  $(".clickable").click(function(){
-    $(".walrus-showing").slideToggle();
-    $(".walrus-hidden").slideToggle();
+  $("#hider").click(function(){
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
+
+  $("#slider").click(function() {
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
+  });
+
+  $("#fader").click(function(){
+    $("#walrus-showing").fadeToggle();
+    $("#walrus-hidden").fadeToggle();
+  })
 });
