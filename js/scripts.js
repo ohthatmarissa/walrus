0112ca581575da712879a530a21689774f40d754
dylan crocker and Marissa Perry <dylan@>
2019-03-26 10:43:41 -0700

    add peek-a-boo

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,6 @@
 $().ready(function(){
-  $("h1").click(function(){
-    alert("This is a header.");
-    alert("SUPER DUPER HEADER");
-    var name = prompt("what's your name?");
-    alert("hello " + name)
-  });
-
-  $("img").click(function(){
-    alert("This is a Walrus.");
-  });
-
-  $("p").click(function(){
-    alert("This is a paragraph");
+  $(".clickable").click(function(){
+    $(".walrus-showing").slideToggle();
+    $(".walrus-hidden").slideToggle();
   });
 });
